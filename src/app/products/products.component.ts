2bf136a6c3536737e34288403408c81012e908a0
Skramblo <97240716+Skramblo@users.noreply.github.com>
2023-05-07 01:07:42 +0200

    fix

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../services/product.service";
 import {Product} from "../model/product.model";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-products',
@@ -9,12 +10,20 @@ import {Product} from "../model/product.model";
 })
 export class ProductsComponent implements OnInit{
   products! : Array<Product> ;
+  currentPage:number=0;
+  totalPages:number=0;
+  pageSize:number=5;
   errorMessage!:string;
-  constructor( private productService : ProductService) {
+  searchFormGroup!:FormGroup;
+  currentAction:string="all";
+  constructor( private productService : ProductService, private fb:FormBuilder) {
 
   }
   ngOnInit():void {
-this.handeleGetAllProducts();
+    this.searchFormGroup=this.fb.group({
+keyword:this.fb.control(null)
+    })
+this.handeleGetPageProducts();
   }
 handeleGetAllProducts(){
   this.productService.getAllProducts().subscribe({
@@ -27,6 +36,18 @@ handeleGetAllProducts(){
     }
   });
 }
+  handeleGetPageProducts(){
+    this.productService.getPageProducts(this.currentPage,this.pageSize).subscribe({
+      next:(data)=>{
+        this.products=data.products;
+        this.totalPages=data.totalPages;
+
+      },
+      error:(err)=>{
+        this.errorMessage=err;
+      }
+    });
+  }
   handleDeleteProduct(p: any) {
     let conf=confirm("Are you sure?");
     if(!conf) return;
@@ -40,15 +61,38 @@ handeleGetAllProducts(){
   }
 
   handlePromotion(p: Product) {
+    let promo=p.promotion;
 this.productService.setPromotion(p.id).subscribe({
   next:(data)=>{
 
-    p.promotion=true;
+    p.promotion=!promo;
   },
   error:err=>{
     this.errorMessage=err;
     }
 })
 
+  }
+
+  handleSearchProducts() {
+    this.currentAction="search";
+    this.currentPage=0;
+let keyword=this.searchFormGroup.value.keyword;
+this.productService.searchProducts(keyword,this.currentPage,this.pageSize).subscribe({
+  next : (data)=>{
+
+    this.products=data.products;
+    this.totalPages=data.totalPages;
+  }
+})
+  }
+
+  gotoPage(i: number) {
+
+    this.currentPage=i;
+    if(this.currentAction=='all')
+    this.handeleGetPageProducts();
+    else
+      this.handleSearchProducts();
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Observable, of, throwError} from "rxjs";
-import {Product} from "../model/product.model";
+import {PageProduct, Product} from "../model/product.model";
+import {UUID} from "angular2-uuid";
 
 @Injectable({
   providedIn: 'root'
@@ -10,25 +11,48 @@ private products! :Array<Product>;
 
   constructor() {
     this.products=[
-      {id:1,name:"computer",price:5000, promotion:true},
-      {id:2,name:"printer",price:444,promotion:false},
-      {id:3,name:"smart phone",price:667,promotion:true}
+      {id:UUID.UUID(),name:"computer",price:5000, promotion:true},
+      {id:UUID.UUID(),name:"printer",price:444,promotion:false},
+      {id:UUID.UUID(),name:"smart phone",price:667,promotion:true}
     ];
+    for(let i=0;i<10;i++){
+      this.products.push({id:UUID.UUID(),name:"computer",price:5000, promotion:true});
+      this.products.push({id:UUID.UUID(),name:"printer",price:444,promotion:false});
+      this.products.push( {id:UUID.UUID(),name:"smart phone",price:667,promotion:false});
+    }
   }
   public getAllProducts(): Observable<Product[]>{
     let rnd=Math.random();
     if(rnd<0.1) return  throwError(()=> new Error("Internet connexion error"))
-    else return of(this.products);
+    else return of([...this.products]);
   }
-  public deleteProduct(id:number): Observable<boolean>{
+  public getPageProducts(page:number,size:number): Observable<PageProduct>{
+    let index=page*size;
+    let totalPages=~~(this.products.length/size);
+    if(this.products.length % size !=0)
+      totalPages++;
+    let pageProduct=this.products.slice(index,index+size);
+    return  of({page:page,size:size,totalPages:totalPages,products:pageProduct});
+  }
+  public deleteProduct(id:string): Observable<boolean>{
     this.products=this.products.filter(p=>p.id!=id);
     return of(true)
   }
-  public setPromotion(id:number): Observable<boolean>{
+  public setPromotion(id:string): Observable<boolean>{
    let product= this.products.find(p=>p.id==id);
     if(product!=undefined) {
       product.promotion = !product.promotion;
       return of(true);
     }else return  throwError(()=>new Error("product not found"));
+  }
+  public  searchProducts(keyword:string,page:number,size:number): Observable<PageProduct>{
+let result=this.products.filter(p=>p.name.includes(keyword));
+
+    let index=page*size;
+    let totalPages=~~(result.length/size);
+    if(this.products.length % size !=0)
+      totalPages++;
+    let pageProduct=result.slice(index,index+size);
+    return  of({page:page,size:size,totalPages:totalPages,products:pageProduct});
   }
 }
